51fdc5ec6212bed17fa71a766d8dd53babc06614
Kosuke Kiuchi <flutcla@gmail.com>
2023-10-10 17:42:33 +0900

    探索の作成中

--- load_hands.py
+++ load_hands.py
@@ -1,8 +1,10 @@
 import glob
+from typing import Any, Generator, Literal
 import yaml
 
 import load_rule
 import load_pies
+from load_pies import Pie
 import load_parts
 
 rule = load_rule.load_rule()
@@ -31,8 +33,25 @@ class Hand():
 
     return True
 
-def load_hands():
-  hands = {}
+  # 牌を send で受け取り、満たすなら StopIteration で True を、満たさないなら False を返すジェネレータ
+  def check_it(this) -> Generator[Literal[True], Pie, bool]:
+    i = 0
+    loc = {}
+    while i < len(this.variables):
+      pie: Pie = yield True
+      loc[this.variables[i]] = pie
+      i += 1
+      for restriction in this.restrictions:
+        try:
+          exec(f'ret = ({restriction})', globals(), loc)
+          if loc['ret'] is False:
+            return False
+        except Exception as e:
+          continue
+    return True
+
+def load_hands() -> dict[str, Hand]:
+  hands: dict[str, Any] = {}
   for fpath in glob.glob('hands/*.yaml'):
     with open(fpath, 'r') as f:
       hands |= yaml.safe_load(f)
@@ -43,3 +62,28 @@ def load_hands():
     hands_func[name] = Hand(restrictions)
 
   return hands_func
+
+def check_hands(pies: list[Pie]):
+  hands = load_hands()
+  result = []
+  for name, hand in hands.items():
+    for pie in pies:
+      if check_rec(hand, [], pie, pies):
+        result.append(name)
+        break
+  return result
+
+def check_rec(hand: Hand, current_pies: list[Pie], next_pie: Pie, left_pies: list[Pie]):
+  current_pies = current_pies.copy()
+  left_pies = left_pies.copy()
+  it = hand.check_it()
+  next(it)
+  current_pies.append(next_pie)
+  left_pies.remove(next_pie)
+  try:
+    [it.send(pie) for pie in current_pies]
+    for pie in left_pies:
+      if check_rec(hand, current_pies, pie, left_pies):
+        return True
+  except StopIteration as e:
+    return e.value
--- load_pies.py
+++ load_pies.py
@@ -6,6 +6,9 @@ class Pie():
     for k, v in kwargs.items():
       setattr(self, k, v)
 
+  def __repr__(self):
+    return f'Pie({self.__dict__})'
+
   def __eq__(self, __value: object) -> bool:
     if not isinstance(__value, Pie):
       return False
